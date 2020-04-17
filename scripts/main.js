c7170d80f4128548592c2f91ac3944f3a7a4c77f
Nolan Nicholson <nolananicholson@gmail.com>
2020-04-17 14:37:14 -0700

    disable anti-aliasing

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("c");
 
-const gl = canvas.getContext("webgl2");
+const gl = canvas.getContext("webgl2", {'antialias': false} );
 if (!gl) {
     console.log("WebGL 2 not supported!");
 }
