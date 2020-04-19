455b6fa1ffaec2218e3b11ff7c2947fdfeeb93c6
Nolan Nicholson <nolananicholson@gmail.com>
2020-04-19 11:52:21 -0700

    rainbow stars!

--- scripts/starfield.js
+++ scripts/starfield.js
@@ -1,31 +1,48 @@
 function starfield() {
-    var tilesize = 1;
-    var tiles_x = 8;
-    var tiles_z = 8;
     var positions = [];
     var colors = [];
 
     var radius = 100
-    var num_stars = 200;
+    var num_stars = 400;
 
     var white = [1, 1, 1];
     var red = [1, 0, 0];
 
     var x; var y; var z;
     var azimuth; var inclination;
+
+    var hue; var chroma; var intermediate;
+    var r; var g; var b;
     
     for (var i = 0; i < num_stars; i++) {
-        //calculate random coordinates on a unit sphere surface
+        //For positions, calculate random coordinates on a unit sphere surface
         z = (Math.random() - 0.5) * 2;
         azimuth = Math.random() * 2 * Math.PI;
         inclination = Math.acos(z);
-        console.log(z, inclination);
-
         x = Math.sin(inclination) * Math.cos(azimuth);
         y = Math.sin(inclination) * Math.sin(azimuth);
 
+        //For colors, calculate a random hue and interpolate that to RGB
+        hue = Math.random() * 6;
+        chroma = 1;
+        intermediate = chroma * (1 - Math.abs(hue % 2 - 1));
+        if (hue <= 1) {
+            r = chroma; g = intermediate; b = 0;
+        } else if (hue <= 2) {
+            r = intermediate; g = chroma; b = 0;
+        } else if (hue <= 3) {
+            r = 0; g = chroma; b = intermediate;
+        } else if (hue <= 4) {
+            r = 0; g = intermediate; b = chroma;
+        } else if (hue <= 5) {
+            r = intermediate; g = 0; b = chroma;
+        } else {
+            r = chroma; g = 0; b = intermediate;
+        }
+
+
         positions.push(x * radius, y * radius, z * radius);
-        colors.push(...white);
+        colors.push(r, g, b);
     }
 
     return [positions, colors]; 
