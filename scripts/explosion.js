499f0a00181d6ed540cb15541b1ef6dbd5559b7a
Nolan Nicholson <nolananicholson@gmail.com>
2020-04-27 12:17:07 -0700

    improve explosion graphic

--- scripts/explosion.js
+++ scripts/explosion.js
@@ -4,8 +4,10 @@ class Explosion extends ObjColor {
         this.program_holder = program_holder_color;
         this.loaded = false;
         this.age = 0;
+        this.max_age = 0.7;
         this.positions = [];
         this.scale = 2;
+        this.palette = [[1, 1, 1], [1, 0, 0], [0, 0, 1], [0, 0, 0]];
 
         //test coords - TODO: remove
         this.x = 2; this.y = 2; this.z = 2;
@@ -37,23 +39,37 @@ class Explosion extends ObjColor {
 
     update(dt) {
         this.age += dt;
+        this.age %= this.max_age;
+        this.scale = 4 * (this.age + 0.25);
+    }
 
-        var new_colors = [];
-        var red = [1, 0, 0];
-        var tri_color;
-        for (var i = 0; i < this.positions.length / 9; i++) {
-            tri_color = [Math.random(), Math.random(), Math.random()];
-            new_colors.push(...tri_color, ...tri_color, ...tri_color);
+    render() {
+        if (this.loaded) {
+            var new_colors = [];
+
+            var color_index;
+            var tri_color;
+            for (var i = 0; i < this.positions.length / 9; i++) {
+
+                color_index = Math.floor(
+                    (this.age / this.max_age * this.palette.length)
+                    + ((Math.random() - 0.5) * 0.5)
+                );
+                color_index = Math.min(color_index, this.palette.length - 1);
+                color_index = Math.max(color_index, 0);
+                tri_color = this.palette[color_index];
+                new_colors.push(...tri_color, ...tri_color, ...tri_color);
+            }
+
+            //update buffers
+            /*
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.position_buffer);
+            gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(positions),
+            */
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.color_buffer);
+            gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(new_colors));
+
+            super.render();
         }
-
-        //update buffers
-        /*
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.position_buffer);
-        gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(positions),
-        */
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.color_buffer);
-        gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(new_colors));
-
     }
 }
