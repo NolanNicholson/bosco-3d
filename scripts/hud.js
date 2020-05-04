2512358b5e2b9a9ba4fec313f7b373b44f093ca1
Nolan Nicholson <nolananicholson@gmail.com>
2020-05-04 10:57:19 -0700

    rendering a cube in the HUD!

--- scripts/hud.js
+++ scripts/hud.js
@@ -16,4 +16,24 @@ function draw_hud() {
     //Reset the GL viewport to the HUD part of the screen
     var hud_view = getHUDViewport(gl.canvas, canv_hud);
     gl.viewport(...hud_view);
+
+    // View-Proj matrix: perspective projection * inverse-camera.
+    var proj_matrix = m4.perspective(
+        1,
+        canv_hud.clientWidth / canv_hud.clientHeight,
+        0.1,
+        2000,
+    );
+    var view_matrix = m4.translation(0, 0, -10);
+    var viewproj = m4.multiply(proj_matrix, view_matrix);
+
+    var ph = program_holder_color;
+    gl.useProgram(ph.program);
+    gl.uniformMatrix4fv(ph.locations.uViewProjMatrixLoc,
+        false, viewproj);
+
+    //Render a cube
+    //var model_matrix = m4.identity();
+    var model_matrix = m4.inverse(player.rotation_matrix);
+    wireframes.cube.render(model_matrix);
 }
